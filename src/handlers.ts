d012a9d073e76cd5eed070f16e0ca13a7f618edc
kanwarsingh <kanwar16112309@gmail.com>
2024-04-04 09:26:03 -0600

    edited handleconnect to not allow duplicate nicknames

--- src/handlers.ts
+++ src/handlers.ts
@@ -8,7 +8,10 @@ type Action =
     | "sendMessage"
     | "$connect"
     | "$disconnect";
-
+type Client = {
+    connectionId: string,
+    nickname: string,
+}
 const CLIENT_TABLE_NAME = "Clients";
 const responseOK = {
     statusCode: 200,
@@ -67,6 +70,8 @@ const handleConnect = async (
         },
     }).promise();
 
+    await notifyClients(connectionId)
+
     return responseOK
 
 };
@@ -81,38 +86,63 @@ const handleDisconnect = async (
         },
     }).promise();
 
+    await notifyClients(connectionId);
+
     return responseOK
 
 };
 
 const notifyClients = async (connectionIdToExclude: string) => {
+    const clients = await getAllClients();
 
+    await Promise.all(clients.filter((client) => client.connectionId !== connectionIdToExclude).map(async (client) =>{
+        await postToConnection(client.connectionId, JSON.stringify(clients))
+    }),
+
+    );
+
+    for (const client of clients) {
+        if(client .connectionId === connectionIdToExclude){
+            continue;
+        }
+        await postToConnection(client.connectionId, JSON.stringify(clients));
+
+    }
 };
 
-const handleGetClients = async (connectionId: string):Promise<APIGatewayProxyResult> =>{
+const getAllClients = async (): Promise<Client[]> => {
     const output = await docClient.scan({
         TableName: CLIENT_TABLE_NAME
     }).promise();
 
     const clients = output.Items || [];
 
+    return clients as Client[]
+}
+
+const postToConnection = async (connectionId: string, data: string) => {
     try {
         await apiGw.postToConnection({
             ConnectionId: connectionId,
-            Data: JSON.stringify(clients),
+            Data: data,
         }).promise();
     }
     catch (e){
         if((e as AWSError).statusCode !== 410){
             throw e;
         }
-            await docClient.delete({
-                TableName: CLIENT_TABLE_NAME,
-                Key:{
-                    connectionId,
-                },
-            }).promise();
-        }
+        await docClient.delete({
+            TableName: CLIENT_TABLE_NAME,
+            Key:{
+                connectionId,
+            },
+        }).promise();
+    }
+}
+const handleGetClients = async (connectionId: string):Promise<APIGatewayProxyResult> =>{
+    const clients = await getAllClients();
+
+    await postToConnection(connectionId, JSON.stringify(clients));
 
     return responseOK;
 };
